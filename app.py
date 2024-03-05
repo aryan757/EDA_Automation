c556780f50b9740114c388b55349e7d3cd786ee0
aryan215gupta <aryan215gupta@gmail.com>
2024-03-05 11:39:45 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.llms import OpenAI
 import pandas as pd
-df = pd.read_csv("C:\\Users\\aryan\\Downloads\\STM_R_and_D\\updated_data.csv")
+#df = pd.read_csv("C:\\Users\\aryan\\Downloads\\STM_R_and_D\\updated_data.csv")
+df = pd.read_csv("./updated_data.csv")
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
--- constant.py
+++ constant.py
@@ -1,1 +1,2 @@
-openai_key="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"
+#openai_key="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"
+openai_key="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"
